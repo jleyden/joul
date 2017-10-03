cd4a0e015c1355e70b122c6a18bd6d7ab4e5c892
John Leyden <johnjl712@gmail.com>
2017-10-02 19:19:21 -0700

    revised profile

--- screens/profile/index.js
+++ screens/profile/index.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Container, Header, Content, List, ListItem, Text, Icon, Left, Body, Right, Switch } from 'native-base';
+import { Container, Header, Content,
+   List, ListItem, Text, Icon, 
+   Left, Body, Right, Switch, Title,
+   Thumbnail } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 
+import defaultPic from './icons/emoji.png'
 
 export default class App extends React.Component {
   render() {
 
     return (
-      <View style={styles.container}>
-        <Container>
-        {/* <Header /> */}
+      <Container style={styles.container}>
+        <Header>
+          <Left>
+            <Text style={styles.rating}>Rating</Text>
+          </Left>
+          <Body>
+            <Title>Username</Title>
+          </Body>
+          <Right>
+            <Text style={styles.money}>$300.00</Text>
+          </Right>
+        </Header>
           <Content>
             <Grid>
-              <Row>
-                <Text style={styles.username}>Username</Text>
-              </Row>
-              <Row>
-                <Text style={styles.money}>$0.00</Text>
-              </Row>
-              <Row>
-                <Text style={styles.rating}>Rating</Text>
+              <Row style={styles.topRow}>
+                <Body>
+                  <Thumbnail large source={defaultPic} />
+                </Body>
               </Row>
               <Row>
                 <List style={styles.list}>
@@ -52,8 +61,7 @@ export default class App extends React.Component {
               </Row>
             </Grid>
           </Content>
-        </Container>
-      </View>
+      </Container>
     );
   }
 }
@@ -62,36 +70,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#242424',
-    // alignItems: 'flex-start',
   },
   username: {
     color: '#FAFAFA',
-    left: 20,
-    top: 30,
     fontSize: 45
   },
   rating: {
-    color: '#BDBDBD',
-    left: 20,
-    top: 25,
-    fontSize: 30
+    color: '#03A9F4',
+    fontSize: 20
   },
   money: {
-    color: '#FAFAFA',
-    left: 20,
-    top: 30,
-    fontSize: 30
+    color: '#009688',
+    fontSize: 20
   },
   item: {
     color: '#FAFAFA',
     padding: 10,
     fontSize: 18,
-    top: 40,
-    left: 20,
-    height: 44,
   },
   list: {
-    top: 40
+    width: '100%'
+  }, 
+  topRow: {
+    padding: 20
   }
-
 });
