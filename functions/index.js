dd2e9ced9623fc4648b7353d0e7b3cff505d5b2e
John Leyden <johnjl712@gmail.com>
2017-11-26 00:00:40 -0500

    tested http requests

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,25 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
+const firebase = require('firebase')
+require('firebase/firestore')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-exports.updateUser = functions.firestore
-	.document('users/{userId}')
-	.onUpdate(event => {
-		// Get an object representing the current document
+exports.validateTrip = functions.firestore
+	.document('users/{userId}/events/{eventId}')
+	.onCreate(event => {
+		// Get an object representing the document
+		// e.g. {'name': 'Marie', 'age': 66}
 		var newValue = event.data.data();
-
-		// ...or the previous value before this update
-		var previousValue = event.data.previous.data();
+		const eventRef = event.data.ref
+		eventRef.update({
+			hello: 'hi'
+		}).then(
+			console.log('sucessfully updated')
+		).catch(function(error) {
+			// The document probably doesn't exist.
+			console.error("Error updating document: ", error);
+		})
 	});
